cf96a404a3d9b73f67d739078da59a89cc316609
mercyasaurus <mercydotexe@gmail.com>
2026-04-12 13:09:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -109,22 +109,21 @@ if mode == "Recipe Directory":
     st.subheader("Recipe Directory")
 
     category = st.selectbox(
-    "Select a category",
-    ["ALL"] + [c.upper() for c in validCats]
-)
+        "Select a category",
+        ["ALL"] + [c.upper() for c in validCats]
+    )
 
-if category.lower() == "all":
-    for recipe in sorted(recipes):
-        st.write(recipe.title())
-else:
-    filtered = [r for r in recipes if recipes[r]["category"] == category.lower()]
+    if category.lower() == "all":
+        for recipe in sorted(recipes):
+            st.write(recipe.title())
+    else:
+        filtered = [r for r in recipes if recipes[r]["category"] == category.lower()]
 
-    for recipe in sorted(filtered):
-        st.write(recipe.title())
+        for recipe in sorted(filtered):
+            st.write(recipe.title())
 
 # ---- CALCULATOR ----
-
-
+else:
     st.subheader("Crafting Calculator")
 
     recipe_input = st.text_input(
@@ -133,7 +132,6 @@ else:
     )
 
     if recipe_input:
-
         separateRecipes = [r.strip() for r in recipe_input.lower().split(",") if r.strip() != ""]
         uniqueRecipes = []
 
@@ -144,13 +142,11 @@ else:
         invalidRecipes = [r for r in uniqueRecipes if r not in recipes]
         validRecipes = [r for r in uniqueRecipes if r in recipes]
 
-        # ---- INVALID ----
         if invalidRecipes:
             st.warning("Invalid recipes will be skipped:")
             for r in invalidRecipes:
                 st.write(r.title())
 
-        # ---- VALID ----
         if validRecipes:
             st.success("Valid recipes selected:")
             for r in validRecipes:
@@ -167,7 +163,6 @@ else:
             )
 
         if st.button("Calculate Grocery List"):
-
             shoppingList = {}
 
             for recipe in validRecipes:
@@ -187,5 +182,5 @@ else:
             st.divider()
             st.subheader("Final Grocery List")
 
-            for ingredient, amount in shoppingList.items():
+            for ingredient, amount in sorted(shoppingList.items()):
                 st.write(f"{ingredient}: {amount}")
